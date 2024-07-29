0b4b52e8cce576b24ce80b75dc441efd5f7c6fb6
im ali <chjczj@126.com>
2024-07-29 19:22:47 +0800

    unplugin init

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import type { UnpluginFactory } from 'unplugin'
 import { createUnplugin } from 'unplugin'
-import type { Options } from './types'
+import type { Options, PartialIconfontConfig } from './types'
+
+export const defineConfig = (configs: PartialIconfontConfig) => configs
 
 export const unpluginFactory: UnpluginFactory<Options | undefined> = options => ({
-  name: 'unplugin-starter',
+  name: 'unplugin-iconfont',
   transformInclude(id) {
-    return id.endsWith('main.ts')
+    return id.endsWith('iconfont.config.', id.length - 2) || id.endsWith('iconfont.config.', id.length - 3)
   },
   transform(code) {
-    return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
+
+    if (!Array.isArray(options))
+      this.error(`Options must be an Array`)
+
+    if (options?.some(o => !o.url))
+      this.error(`Options url parameter is required`)
+
+    // console.log(chalk.yellow.inverse(' code '), code)
+    // return
+
+    // return code.replace('__UNPLUGIN__', `Hello Unplugin! ${options}`)
   },
 })
 
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,49 @@
-export interface Options {
-  // define your plugin options here
+export interface IconfontConfig {
+  /**
+   * iconfont symbol js url
+   */
+  url: string
+  /**
+   * 保存自动下载iconfont symbol js的public下的路径，默认为iconfonts
+   */
+  filePath?: string
+  /**
+   * 自动下载iconfont symbol js的文件名称，默认为url最后一个/后的名称
+   */
+  fileName?: string
+  /**
+   * iconfont symbol js是否自动注入到index.html
+   */
+  inject?: boolean
+  /**
+   * 是否生成icon类型声明文件，可以为boolean或者具体生成的路径
+   */
+  dts?: boolean | string
+  /**
+   * 自动生成iconfont图标集合
+   */
+  iconJson?: boolean | string
+  /**
+   * 是否添加prefix前缀
+   * @example prefix: 'icon-' 生成的symbol id为 icon-xxx
+   */
+  prefix?: string
+  /**
+   * 前缀分隔符
+   * @default -
+   */
+  separator?: string
+  /**
+   * 对iconfont symbol进行trim start
+   * @example trimStart: 'icon-' 'icon-xxx' 生成的symbol id为 xxx
+   */
+  trimStart?: string
+  /**
+   * 是否输出 iconify 格式的 json，iconJson 为 false 时无效
+   */
+  iconifyJson?: boolean
 }
+
+export type PartialIconfontConfig = IconfontConfig | IconfontConfig[]
+
+export type Options = { configFile?: string } | PartialIconfontConfig
